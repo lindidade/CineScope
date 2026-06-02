8bbddb4c5dc8a7ca7019a5fef9fb32d6d6de3d0e
Linh <sigma@DESKTOP-TLVUI6R>
2026-06-02 08:55:53 +0200

    Add project files.

---
+++ Data/CineScopeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CineScope.Models;
+
+namespace CineScope.Data
+{
+    public class CineScopeDbContext : DbContext
+    {
+        public CineScopeDbContext(DbContextOptions<CineScopeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace CineScope.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+        public double Rating { get; set; }
+        public int Duration { get; set; }
+        public string PosterUrl { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
